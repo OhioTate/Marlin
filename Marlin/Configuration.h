eade3dc78d8d2bd00dfa485a8e098da54030d083
OhioTate <ohiotatedev@gmail.com>
2023-07-04 11:44:01 -0400

    modified bltouch offset for new satsana abs fan shroud.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1538,7 +1538,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -48, -13, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -48, -10.5, 0 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
